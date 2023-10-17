28ab8c5f14bb9c95672eebf0820075be853b7c42
Elizabeth <elizabethadebayo269@gmail.com>
2023-10-17 14:59:06 +0100

    maade some changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
   console.log('we are about to reveal the employees')
   const employee = true;
+  const [role, setRole] = useState('dev'); //dev is a default variable
+
   return (
     <div className="App">
       <header className="App-header">
         {employee ? 
-        <>
+        (
+          <>
+          <input type = 'text' onChange={(e) => {console.log(e.target.value);
+          setRole(e.target.value);}}/>
           <Employee name='Elizabeth' role='intern'/>
           <Employee name='jane' role='intern'/>
-          <Employee name='lierr'/>
+          <Employee name='lierr' role={role}/>
           <Employee name='bread'/>
           <Employee name= 'beans'/>
           <Employee name= 'butter'/>
-        </>
+          </>
+
+        )
+        
         :
         'we cannot show YOU the employees'
         }
